a8f49b909e5e155d2081a450163425acdb833202
vong-e <sinkim123321@gmail.com>
2021-09-29 03:37:19 +0900

    - 순열과 조합 하나로 합침

---
+++ Algorithm/PermutationAndCombination.playground/Pages/Combination.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+/*
+ for loop를 사용한 조합 찾기
+ */
+let numberArray: [Int] = [1, 2, 3, 4]
+
+func combinationWithForLoop(array: [Int]) {
+
+}
--- Algorithm/PermutationAndCombination.playground/Pages/Permutation.xcplaygroundpage/Contents.swift
+++ Algorithm/PermutationAndCombination.playground/Pages/Permutation.xcplaygroundpage/Contents.swift
@@ -18,7 +18,7 @@ func permutationWithForLoop(array: [Int]) {
             }
         }
     }
-    print("4개의 값에서 3개를 고르는 경우의 수: \(permutaionArray.count)개")
+    print("4개의 값에서 3개를 고르는 순열의 수: \(permutaionArray.count)개")
     print(permutaionArray)
 }
 
@@ -30,7 +30,7 @@ func permutationWithForLoop(array: [Int]) {
  */
 let stringArray: [String] = ["A", "B", "C", "D"]
 var permutaionArray: [[String]] = [] //찾은 순열을 저장할 배열
-var numberOfPick: Int = 3 //몇개로 구성된 순열을 찾을지
+var numberOfPick: Int = 2 //몇개로 구성된 순열을 찾을지
 
 func permutaionWithRecursive(array: [String], pickCount: Int, permutationArray: inout [[String]], index: Int = 0) {
     if index == pickCount {
@@ -47,12 +47,12 @@ func permutaionWithRecursive(array: [String], pickCount: Int, permutationArray: 
     }
 }
 
-//permutaionWithRecursive(array: stringArray, pickCount: numberOfPick, permutationArray: &permutaionArray)
-//print("4개의 값에서 3개를 고르는 경우의 수: \(permutaionArray.count)개") //24개
-//print(permutaionArray)
+permutaionWithRecursive(array: stringArray, pickCount: numberOfPick, permutationArray: &permutaionArray)
+print("4개의 값에서 \(numberOfPick)개를 고르는 순열의 수: \(permutaionArray.count)개") //24개
+print(permutaionArray)
 /*
-[["A", "B", "C"], ["A", "B", "D"], ["A", "C", "B"], ["A", "C", "D"], ["A", "D", "C"], ["A", "D", "B"],
- ["B", "A", "C"], ["B", "A", "D"], ["B", "C", "A"], ["B", "C", "D"], ["B", "D", "C"], ["B", "D", "A"],
- ["C", "B", "A"], ["C", "B", "D"], ["C", "A", "B"], ["C", "A", "D"], ["C", "D", "A"], ["C", "D", "B"],
- ["D", "B", "C"], ["D", "B", "A"], ["D", "C", "B"], ["D", "C", "A"], ["D", "A", "C"], ["D", "A", "B"]]
+[["A", "B"], ["A", "C"], ["A", "D"],
+ ["B", "A"], ["B", "C"], ["B", "D"],
+ ["C", "B"], ["C", "A"], ["C", "D"],
+ ["D", "B"], ["D", "C"], ["D", "A"]]
  */
